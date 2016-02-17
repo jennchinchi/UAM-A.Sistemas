80f75787d2eee572048d1285fbd3ee90c2d3bb76
Jesus <jesus.rojas.umana@gmail.com>
2016-02-17 00:21:00 -0600

    Avance en controladores mvc - jesus, oscar, jose

---
+++ TiendaVirtual/TiendaVirtual/TiendaVirtual/Controllers/tb_productoController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TiendaVirtual.Models;
+
+namespace TiendaVirtual.Controllers
+{
+    public class tb_productoController : Controller
+    {
+        private bd_tienda_virtual_dellEntities db = new bd_tienda_virtual_dellEntities();
+
+        // GET: tb_producto
+        public ActionResult Index()
+        {
+            var tb_producto = db.tb_producto.Include(t => t.tb_categoria).Include(t => t.tb_estado);
+            return View(tb_producto.ToList());
+        }
+
+        // GET: tb_producto/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tb_producto tb_producto = db.tb_producto.Find(id);
+            if (tb_producto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tb_producto);
+        }
+
+        // GET: tb_producto/Create
+        public ActionResult Create()
+        {
+            ViewBag.id_categoria_prod = new SelectList(db.tb_categoria, "id_categoria", "descripcion");
+            ViewBag.id_estado = new SelectList(db.tb_estado, "id_estado", "descripcion");
+            return View();
+        }
+
+        // POST: tb_producto/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id_producto,nombre_prod,costo,cantidad,descripcion_prod,id_categoria_prod,id_estado,imagen")] tb_producto tb_producto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tb_producto.Add(tb_producto);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.id_categoria_prod = new SelectList(db.tb_categoria, "id_categoria", "descripcion", tb_producto.id_categoria_prod);
+            ViewBag.id_estado = new SelectList(db.tb_estado, "id_estado", "descripcion", tb_producto.id_estado);
+            return View(tb_producto);
+        }
+
+        // GET: tb_producto/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tb_producto tb_producto = db.tb_producto.Find(id);
+            if (tb_producto == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.id_categoria_prod = new SelectList(db.tb_categoria, "id_categoria", "descripcion", tb_producto.id_categoria_prod);
+            ViewBag.id_estado = new SelectList(db.tb_estado, "id_estado", "descripcion", tb_producto.id_estado);
+            return View(tb_producto);
+        }
+
+        // POST: tb_producto/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id_producto,nombre_prod,costo,cantidad,descripcion_prod,id_categoria_prod,id_estado,imagen")] tb_producto tb_producto)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tb_producto).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.id_categoria_prod = new SelectList(db.tb_categoria, "id_categoria", "descripcion", tb_producto.id_categoria_prod);
+            ViewBag.id_estado = new SelectList(db.tb_estado, "id_estado", "descripcion", tb_producto.id_estado);
+            return View(tb_producto);
+        }
+
+        // GET: tb_producto/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tb_producto tb_producto = db.tb_producto.Find(id);
+            if (tb_producto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tb_producto);
+        }
+
+        // POST: tb_producto/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            tb_producto tb_producto = db.tb_producto.Find(id);
+            db.tb_producto.Remove(tb_producto);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ TiendaVirtual/TiendaVirtual/TiendaVirtual/Controllers/tb_tipo_usuarioController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TiendaVirtual.Models;
+
+namespace TiendaVirtual.Controllers
+{
+    public class tb_tipo_usuarioController : Controller
+    {
+        private bd_tienda_virtual_dellEntities db = new bd_tienda_virtual_dellEntities();
+
+        // GET: tb_tipo_usuario
+        public ActionResult Index()
+        {
+            return View(db.tb_tipo_usuario.ToList());
+        }
+
+        // GET: tb_tipo_usuario/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tb_tipo_usuario tb_tipo_usuario = db.tb_tipo_usuario.Find(id);
+            if (tb_tipo_usuario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tb_tipo_usuario);
+        }
+
+        // GET: tb_tipo_usuario/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: tb_tipo_usuario/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id_tipo_usuario,descripcion")] tb_tipo_usuario tb_tipo_usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tb_tipo_usuario.Add(tb_tipo_usuario);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tb_tipo_usuario);
+        }
+
+        // GET: tb_tipo_usuario/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tb_tipo_usuario tb_tipo_usuario = db.tb_tipo_usuario.Find(id);
+            if (tb_tipo_usuario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tb_tipo_usuario);
+        }
+
+        // POST: tb_tipo_usuario/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id_tipo_usuario,descripcion")] tb_tipo_usuario tb_tipo_usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tb_tipo_usuario).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tb_tipo_usuario);
+        }
+
+        // GET: tb_tipo_usuario/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tb_tipo_usuario tb_tipo_usuario = db.tb_tipo_usuario.Find(id);
+            if (tb_tipo_usuario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tb_tipo_usuario);
+        }
+
+        // POST: tb_tipo_usuario/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            tb_tipo_usuario tb_tipo_usuario = db.tb_tipo_usuario.Find(id);
+            db.tb_tipo_usuario.Remove(tb_tipo_usuario);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
