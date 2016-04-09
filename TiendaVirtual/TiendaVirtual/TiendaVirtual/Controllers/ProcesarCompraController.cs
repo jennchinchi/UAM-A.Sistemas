6b630a60f5a092cc44c3faa4af6dcd67fc1fa07e
jennchinchi <jechipo@hotmail.com>
2016-04-09 01:10:06 -0600

    Listar carritos, procesar compra incompleto
    Listar carritos, procesar compra incompleto

---
+++ TiendaVirtual/TiendaVirtual/TiendaVirtual/Controllers/ProcesarCompraController.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using TiendaVirtual.Configuracion;
+using TiendaVirtual.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TiendaVirtual.Controllers
+{
+    [Authorize]
+    public class CheckoutController : Controller
+    {
+        private bd_tienda_virtual_dellEntities db = new bd_tienda_virtual_dellEntities();
+        AppConfigurations appConfig = new AppConfigurations();
+
+        public List<String> CreditCardTypes { get { return appConfig.CreditCardType; } }
+
+        //
+        // GET: /Checkout/AddressAndPayment
+        public ActionResult AddressAndPayment()
+        {
+            ViewBag.CreditCardTypes = CreditCardTypes;
+            var previousOrder = db.tb_factura.FirstOrDefault(x => x.usuario == User.Identity.Name);
+
+            if (previousOrder != null)
+                return View(previousOrder);
+            else
+                return View();
+        }
+
+        //
+        // POST: /Checkout/AddressAndPayment
+        [HttpPost]
+        public async Task<ActionResult> AddressAndPayment(FormCollection values)
+        {
+            ViewBag.CreditCardTypes = CreditCardTypes;
+            string result = values[9];
+
+            var order = new tb_factura();
+            TryUpdateModel(order);
+            //order.CreditCard = result;
+
+            try
+            {
+                order.usuario = User.Identity.Name;
+                order.fecha = DateTime.Now;
+                var currentUserId = User.Identity.GetUserId();
+
+                //if (order.SaveInfo && !order.Username.Equals("guest@guest.com"))
+                //{
+
+                //    var manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
+                //    var store = new UserStore<ApplicationUser>(new ApplicationDbContext());
+                //    var ctx = store.Context;
+                //    var currentUser = manager.FindById(User.Identity.GetUserId());
+
+                //    currentUser.Address = order.Address;
+                //    currentUser.City = order.City;
+                //    currentUser.Country = order.Country;
+                //    currentUser.State = order.State;
+                //    currentUser.Phone = order.Phone;
+                //    currentUser.PostalCode = order.PostalCode;
+                //    currentUser.FirstName = order.FirstName;
+
+                //    //Save this back
+                //    //http://stackoverflow.com/questions/20444022/updating-user-data-asp-net-identity
+                //    //var result = await UserManager.UpdateAsync(currentUser);
+                //    await ctx.SaveChangesAsync();
+
+                //    await db.SaveChangesAsync();
+                //}
+
+
+                //Save Order
+                db.tb_factura.Add(order);
+                await db.SaveChangesAsync();
+                //Process the order
+                var cart = ShoppingCart.GetCart(this.HttpContext);
+                order = cart.CreateOrder(order);
+
+
+
+                //CheckoutController.SendOrderMessage(order.usuario, "Nueva Factura: " + order.id_factura);
+
+                return RedirectToAction("Completada",
+                    new { id = order.id_factura });
+
+            }
+            catch
+            {
+                //Invalid - redisplay with errors
+                return View(order);
+            }
+        }
+
+        //
+        // GET: /Checkout/Complete
+        public ActionResult Complete(int id)
+        {
+            // Validate customer owns this order
+            bool isValid = db.tb_factura.Any(
+                o => o.id_factura == id &&
+                o.usuario == User.Identity.Name);
+
+            if (isValid)
+            {
+                return View(id);
+            }
+            else
+            {
+                return View("Error");
+            }
+        }
+        
+    }
+}
---
+++ TiendaVirtual/TiendaVirtual/TiendaVirtual/Controllers/tb_facturaController.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PagedList.Mvc;
+using PagedList;
+using TiendaVirtual.Models;
+
+namespace TiendaVirtual.Controllers
+{
+    public class tb_facturaController : Controller
+    {
+        private bd_tienda_virtual_dellEntities db = new bd_tienda_virtual_dellEntities();
+
+
+        // GET: Orders
+        public ActionResult Index(string sortOrder, string currentFilter, string searchString, int? page)
+        {
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewBag.PriceSortParm = sortOrder == "Price" ? "price_desc" : "Price";
+
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewBag.CurrentFilter = searchString;
+
+            var orders = from o in db.tb_factura
+                         select o;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                orders = orders.Where(s => s.usuario.ToUpper().Contains(searchString.ToUpper()));
+            }
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    orders = orders.OrderByDescending(s => s.usuario);
+                    break;
+                case "Price":
+                    orders = orders.OrderBy(s => s.costo_total);
+                    break;
+                case "price_desc":
+                    orders = orders.OrderByDescending(s => s.costo_total);
+                    break;
+                default:  // Name ascending 
+                    orders = orders.OrderBy(s => s.usuario);
+                    break;
+            }
+
+            int pageSize = 3;
+            int pageNumber = (page ?? 1);
+            return View(orders.ToPagedList(pageNumber, pageSize));
+
+            //return View(await db.Orders.ToListAsync());
+        }
+
+        // GET: Orders/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tb_factura order = await db.tb_factura.FindAsync(id);
+            var orderDetails = db.tb_detalle.Where(x => x.id_factura == id);
+
+            order.tb_detalle = await orderDetails.ToListAsync();
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order);
+        }
+
+        // GET: Orders/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Orders/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(tb_factura order)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tb_factura.Add(order);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(order);
+        }
+
+        // GET: Orders/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tb_factura order = await db.tb_factura.FindAsync(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order);
+        }
+
+        // POST: Orders/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(tb_factura order)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(order).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(order);
+        }
+
+        // GET: Orders/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tb_factura order = await db.tb_factura.FindAsync(id);
+            if (order == null)
+            {
+                return HttpNotFound();
+            }
+            return View(order);
+        }
+
+        // POST: Orders/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            tb_factura order = await db.tb_factura.FindAsync(id);
+            db.tb_factura.Remove(order);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
